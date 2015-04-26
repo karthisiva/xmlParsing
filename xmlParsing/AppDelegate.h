b018730ee241d68e90916fe5f7cd959015240a90
karthisiva <GriffinInfosystems1@mani-MacBook-Pro-2.local>
2015-04-26 20:55:05 +0530

    Initial Commit

---
+++ xmlParsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xmlParsing
+//
+//  Created by Griffin on 26/04/15.
+//  Copyright (c) 2015 Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xmlParsing/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  xmlParsing
+//
+//  Created by Griffin on 26/04/15.
+//  Copyright (c) 2015 Griffin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
